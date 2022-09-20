3797185b38ebf2564b7a6290e1ed1ccb74ab7e8e
eschencpp <eschen@cpp.edu>
2022-09-20 14:29:42 -0700

    12.8 Added 5 second loading delay

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -9,9 +9,12 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 
@@ -23,11 +26,16 @@ class CrimeListFragment : Fragment() {
         }
             private val crimeListViewModel: CrimeListViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+        viewLifecycleOwner.lifecycleScope.launch{
+            val crimes = crimeListViewModel.loadCrimes()
+            binding.crimeRecyclerView.adapter =
+                CrimeListAdapter(crimes)
+        }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,9 +45,6 @@ class CrimeListFragment : Fragment() {
         _binding = FragmentCrimeListBinding.inflate(inflater, container,false)
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.crimeRecyclerView.adapter = adapter
 
         return binding.root
     }
